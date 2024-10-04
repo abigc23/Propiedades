5259bb79141d838c3034576bc7ca7b22ec2ce903
abigc23 <rociogutierrezcao@gmail.com>
2024-10-04 00:53:05 -0300

    limpieza de codigo y de archivos

--- App/urls.py
+++ App/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-#-->Importamos las Vistas para las URL
 from .views import *
 
 urlpatterns = [
     #-->URL, FUNCION, operacion PARA HTML
     path('',Home,name='inicio'),
-    path('agregar/',Agregar,name='agregar'),
     path('propiedades/',ver_Propiedades,name='propiedades'),
-    path('modificar/<codigo>/',Modificar_Propiedad,name='modificar'),
-    path('eliminar/<codigo>/',Eliminar_Propiedad,name='eliminar'),
-    path('logouts/',salir,name='logouts'),
+    path('detallesprop/',ver_Propiedades,name='detallesprop'),
 ]
--- App/views.py
+++ App/views.py
@@ -1,11 +1,6 @@
-from django.shortcuts import render,get_object_or_404,redirect
-#---->Importamos el Sector de Formularios
+from django.shortcuts import render 
 from .forms import *
 from .models import *
-#--->Importamos la Libreria de Logout
-from django.contrib.auth import logout
-#--->Importamos la Libreria de Permisos
-from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
 def Home(request):
@@ -16,53 +11,8 @@ def Home(request):
     return render (request,'index.html',data)
 
 def ver_Propiedades(request):
-    #--->TREAMOS TODOS LOS ELEMENTOS DEL TABLA
     buscar=Propiedad.objects.all()
     data={
         'forms':buscar
     }
-    return render(request,'Pages/propiedades.html',data)
-
-@login_required
-
-@permission_required('App.add_propiedad')
-def Agregar(request):
-    data={
-        'forms':NuevaPropiedad()
-    }
-    if request.method=='POST':
-        query=NuevaPropiedad(data=request.POST,files=request.FILES)
-        if  query.is_valid():
-            query.save()
-            data['mensaje']="Datos Registrados"
-        else:
-            data['forms']=NuevaPropiedad
-    return render (request,'Pages/agregar.html',data)
-
-
-@permission_required('App.change_propiedad')
-def Modificar_Propiedad(request,Codigo):
-    sql=get_object_or_404(Propiedad,Codigo=Codigo)
-    data={
-        'forms':NuevaPropiedad(instance=sql)
-    }
-    if request.method=='POST':
-        query=NuevaPropiedad(data=request.POST,instance=sql,files=request.FILES)
-        if  query.is_valid():
-            query.save()
-            data['mensaje']="Datos Modificados Correctamente "
-        else:
-            data['forms']=NuevaPropiedad
-    return render (request,'Pages/modificar.html',data)
-
-
-@permission_required('App.delete_propiedad')
-def Eliminar_Propiedad(request,Codigo):
-    buscar=get_object_or_404(Propiedad,Codigo=Codigo)
-    buscar.delete()
-    return redirect(to="propiedades")
-
-
-def salir(request):
-    logout(request)
-    return redirect(to='inicio')
+    return render(request,'pages/propiedades.html',data)
